99ea569b1d4cc3b0ac4a6f4534aac7243d5fc55d
Roger Binns <rogerb@rogerbinns.com>
2024-06-02 08:20:33 -0700

    Don't try to run configure with msvc

--- setup.py
+++ setup.py
@@ -852,18 +852,20 @@ class mc_build_ext(apsw_build_ext):
             ext.define_macros.append(("SQLITE3MC_OMIT_AES_HARDWARE_SUPPORT", "1"))
 
         # configure was not run because we didn't download the
-        # amalgamation.  run it here.
-        proc = subprocess.run(["env", f"CC={shlex.join(self.compiler.compiler)}",
-                               "./configure"], text=True,
-                              capture_output=True, cwd="sqlite3/configure/")
-        if proc.returncode == 0:
-            for line in open("sqlite3/configure/Makefile"):
-                if line.startswith("DEFS ="):
-                    for part in  shlex.split(line):
-                        if part.startswith("-DHAVE"):
-                            parts = part.split("=", 1)
-                            ext.define_macros.append((parts[0][2:], parts[1]))
-                    break
+        # amalgamation.  run it here.  msvc doesn't have compiler nor
+        # would configure run anyway
+        if hasattr(self.compiler, "compiler"):
+            proc = subprocess.run(["env", f"CC={shlex.join(self.compiler.compiler)}",
+                                "./configure"], text=True,
+                                capture_output=True, cwd="sqlite3/configure/")
+            if proc.returncode == 0:
+                for line in open("sqlite3/configure/Makefile"):
+                    if line.startswith("DEFS ="):
+                        for part in  shlex.split(line):
+                            if part.startswith("-DHAVE"):
+                                parts = part.split("=", 1)
+                                ext.define_macros.append((parts[0][2:], parts[1]))
+                        break
 
         # We always want temp store in memory and secure delete
         ext.define_macros.append(("SQLITE_TEMP_STORE", "2"))
