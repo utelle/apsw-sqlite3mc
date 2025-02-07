4d625e41966e64bfbfcb08ce14210d061b322b9f
utelle <ulrich.telle@gmx.de>
2025-02-07 23:50:55 +0100

    Update sqlite3mc amalgamation to version 2.0.3
    - remove old configure support
    - add autosetup support
    - adjust GitHub workflows

--- sqlite3/configure/sqlite3.c
+++
@@ -1,3 +0,0 @@
-dummy file - if not present in the same directory
-as configure then configure looks around the rest of the filesystem
-for its helper scripts and gets upset when it can't find them.
